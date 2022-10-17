7213557bbb4b67f2a339ad151cb9aae377d81f8e
Rafael­Manzo <prormanzo@gmail.com>
2022-10-17 14:48:48 -0700

    Here it goes!..

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Rafael Manzo 
+Combine Level One
+Monday October 17 2022
+This program is an update to our old code from the combie. We will be ading data validation and pushing our code to our GitHub repostiories
+                                --Hello World--
+*/
+
+        Console.Clear();
+
+        Console.WriteLine("hello what is your name");
+
+        string name1 = Console.ReadLine();
+
+        Console.WriteLine("hello " + name1 + " nice to meet you." );
