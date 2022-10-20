52c6818181778649282a8dc2379f9b0aaf3f1e46
Reed Goodwin <113049336+Soup-Mann@users.noreply.github.com>
2022-10-20 11:08:51 -0700

    PeerReview

--- Program.cs
+++ Program.cs
@@ -4,12 +4,24 @@ Combine Level One
 Monday October 17 2022
 This program is an update to our old code from the combie. We will be ading data validation and pushing our code to our GitHub repostiories
                                 --Hello World--
-*/
-
+*/      
+        string stop = "";
         Console.Clear();
-
+        while(stop != "no"){
         Console.WriteLine("hello what is your name");
 
         string name1 = Console.ReadLine();
+        Console.WriteLine("" );
+        Console.WriteLine("" );
 
         Console.WriteLine("hello " + name1 + " nice to meet you." );
+        Console.WriteLine("" );
+        Console.WriteLine("" );
+
+        Console.WriteLine("Play Again?" );
+        Console.WriteLine("Type anything to restart || Type \"no\" to end program" );
+        stop = Console.ReadLine();
+        Console.WriteLine("" );
+        Console.WriteLine("" );
+        
+        }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RmanzoMini1Git
+
+Peer Review:Reed - Simple and effective, Would have added validation. PLay it again loop works well.
